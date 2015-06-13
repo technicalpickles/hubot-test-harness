e326692dad1a1949a3469ac505fa8988d1779016
Michael Ansel <mansel@box.com>
2015-06-13 11:55:49 -0700

    Add support for development versions of Hubot

--- build-a-bot.sh
+++ build-a-bot.sh
@@ -4,3 +4,6 @@
 mkdir hubot-under-test
 cd hubot-under-test
 yo hubot:app --adapter=irc --defaults
+
+# Hack to install an 'npm link'ed version of Hubot
+npm install "$(npm link hubot | awk -F"->" '{print $NF}' | tail -n 1)"
